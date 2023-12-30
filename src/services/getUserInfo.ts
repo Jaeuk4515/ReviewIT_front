cb2adc7e605861cc1364f98d26ae3f31213b4029
jaeuk <jojeon4515@gmail.com>
2023-12-30 11:56:18 +0900

    feat: 쿠키 수정

--- src/services/getUserInfo.ts
+++ src/services/getUserInfo.ts
@@ -3,7 +3,7 @@ import Cookies from 'universal-cookie';
 import { origin_URL } from '../App';
 
 export default async function getUserInfo() {
-  const cookies = new Cookies(null, { path: '/' });
+  const cookies = new Cookies(null, { path: '/', secure: true, sameSite: 'none' });
   const jwtToken = cookies.get('token');
   console.log(jwtToken);
   
