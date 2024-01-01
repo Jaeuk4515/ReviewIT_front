8b6a0120cd5987fe02ad3b3b101ba8095656b263
jaeuk <jojeon4515@gmail.com>
2024-01-01 13:19:10 +0900

    feat: 테마에 따른 로그인 경고창 로고 색 수정

--- src/components/blocks/Modal/RequireLoginModal/RequireLoginModal.tsx
+++ src/components/blocks/Modal/RequireLoginModal/RequireLoginModal.tsx
@@ -1,13 +1,16 @@
 import { Logo, ModalBg, ModalButton } from "../AuthModal/AuthModal.styles";
 import { ModalText } from "./RequireLoginModal.styles";
 import { ModalBox } from "../SuccessModal/SuccessModal.styles";
-import { ButtonArea } from "../../Header/Header.styles";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../store/RootState";
 
 export default function RequireLoginModal({ setLoginRequired }: {setLoginRequired: React.Dispatch<React.SetStateAction<boolean>>}) {
+  const { theme } = useSelector((state: RootState) => state.theme);
+
   return (
     <ModalBg>
       <ModalBox style={{width: "420px", height: "320px", gap: '2.5rem'}}>
-        <Logo />
+        <Logo theme={theme} />
         <ModalText>로그인이 필요한 서비스입니다!</ModalText>
         <ModalButton style={{width: "70%"}} onClick={() => { setLoginRequired(false) }}>확인</ModalButton>
       </ModalBox>
