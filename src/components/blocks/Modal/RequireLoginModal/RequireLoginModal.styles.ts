29bfc33268a50f866b75e238caa4cc781be944b1
jaeuk <jojeon4515@gmail.com>
2024-01-08 13:37:43 +0900

    style: 로그인 요청 모달창 반응형 스타일 적용

--- src/components/blocks/Modal/RequireLoginModal/RequireLoginModal.styles.ts
+++ src/components/blocks/Modal/RequireLoginModal/RequireLoginModal.styles.ts
@@ -1,15 +1,47 @@
 import styled from "styled-components";
-import { Img } from "../../../atoms/Category/Category.styles";
+import alert from "../../../../assets/icons/alert.svg";
 import { ModalTitle } from "../AuthModal/AuthModal.styles";
+import { ModalBox, SuccessMark } from "../SuccessModal/SuccessModal.styles";
+
+const NewModalBox = styled(ModalBox)`
+  min-width: 270px;
+  height: 360px;
+
+  @media ${({ theme }) => theme.mediaQuery.small} {
+    height: 300px;
+  };
+  @media screen and (max-width: 450px) {
+    width: 310px;
+    height: 250px;
+  };
+  @media screen and (max-width: 400px) {
+    width: 270px;
+    height: 200px;
+  };
+`
 
 const ModalText = styled(ModalTitle)`
   display: flex;
   gap: 1rem;
 `
 
-const AlertIcon = styled(Img)`
-  width: 30px;
-  height: 30px;
+const AlertIcon = styled(SuccessMark)`
+  background-image: url(${alert});
+  width: 26px;
+  height: 26px;
+
+  @media ${({ theme }) => theme.mediaQuery.small} {
+    width: 22px;
+    height: 22px;
+  };
+  @media screen and (max-width: 450px) {
+    width: 18px;
+    height: 18px;
+  };
+  @media screen and (max-width: 400px) {
+    width: 14px;
+    height: 14px;
+  };
 `
 
-export { ModalText, AlertIcon };
+export { NewModalBox, ModalText, AlertIcon };
--- src/components/blocks/Modal/RequireLoginModal/RequireLoginModal.tsx
+++ src/components/blocks/Modal/RequireLoginModal/RequireLoginModal.tsx
@@ -1,19 +1,22 @@
-import { Logo, ModalBg, ModalButton } from "../AuthModal/AuthModal.styles";
-import { ModalText } from "./RequireLoginModal.styles";
-import { ModalBox } from "../SuccessModal/SuccessModal.styles";
+import { ModalBg } from "../AuthModal/AuthModal.styles";
+import { ConfirmButton, ModalBox, ModalLogo, SuccessMessage, SuccessText } from "../SuccessModal/SuccessModal.styles";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../../store/RootState";
+import { NewModalBox, AlertIcon } from "./RequireLoginModal.styles";
 
 export default function RequireLoginModal({ setLoginRequired }: {setLoginRequired: React.Dispatch<React.SetStateAction<boolean>>}) {
   const { theme } = useSelector((state: RootState) => state.theme);
 
   return (
     <ModalBg>
-      <ModalBox style={{width: "420px", height: "320px", gap: '2.5rem'}}>
-        <Logo logotheme={theme} />
-        <ModalText>로그인이 필요한 서비스입니다!</ModalText>
-        <ModalButton style={{width: "70%"}} onClick={() => { setLoginRequired(false) }}>확인</ModalButton>
-      </ModalBox>
+      <NewModalBox>
+        <ModalLogo logotheme={theme} />
+        <SuccessMessage>
+          <AlertIcon logotheme={theme} />
+          <SuccessText>로그인이 필요한 서비스입니다!</SuccessText>
+        </SuccessMessage>
+        <ConfirmButton style={{width: "70%"}} onClick={() => { setLoginRequired(false) }}>확인</ConfirmButton>
+      </NewModalBox>
     </ModalBg>
   )
 }
